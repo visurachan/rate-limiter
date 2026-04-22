1535512882d13a0dc4df80cec81d9880878230e5
Visura Chandula <visurachandula@gmail.com>
2026-04-22 13:00:21 +0100

    chore: initial project setup with Spring Boot, Redis, PostgreSQL

---
+++ src/main/java/com/ratelimitter/rl_service/RlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ratelimitter.rl_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratelimitter/rl_service/RlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratelimitter.rl_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
